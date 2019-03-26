32cf0aa9c76ce7fdfedab3888a9acfdf5fac33e7
nontapong <reventon11@outlook.com>
2019-03-26 17:11:33 +0700

    ดึงข้อมูลลงตาราง report

--- routes/ReportRouter.js
+++ routes/ReportRouter.js
@@ -3,6 +3,7 @@ const app = express();
 const ReportRouter = express.Router();
 const License = require('../models/License.model');
 const sell = require('../models/TRN_sell_car.model');
+const repair = require('../models/TRN_repair_car.model.js')
 
 
 ReportRouter.route('/').get(function (req, res) {
@@ -15,12 +16,18 @@ ReportRouter.route('/').get(function (req, res) {
         if (err) {
           console.log(err);      
         }else {
-          res.render('Report', { license: license , sells : sells});
+          repair.find(function (err, repairs){
+            if (err) {
+              console.log(err);      
+            }else {
+              res.render('Report', { license: license , sells : sells , repairs : repairs});
+            }
+          })
         }
       })
       // res.send(license)
     }
   });
 });
-
+// res.render('Report', { license: license , sells : sells});
 module.exports = ReportRouter;
